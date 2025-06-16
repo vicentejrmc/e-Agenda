e1c0f8b9213c316a6a8e10f80c45fb05f09a3b14
Iagodizlla <iagoschlemper@gmail.com>
2025-06-16 15:51:47 -0300

    IMplementado classes de compartilhado de dominio

---
+++ e-Agenda.Dominio/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,8 @@
+﻿namespace ControleDeBar.Dominio.Compartilhado;
+
+public abstract class EntidadeBase<T>
+{
+    public Guid Id { get; set; }
+
+    public abstract void AtualizarRegistro(T registroEditado);
+}
---
+++ e-Agenda.Dominio/Compartilhado/IRepositorio.cs
@@ -0,0 +1,14 @@
+﻿namespace ControleDeBar.Dominio.Compartilhado;
+
+public interface IRepositorio<T> where T : EntidadeBase<T>
+{
+    public void CadastrarRegistro(T novoRegistro);
+
+    public bool EditarRegistro(Guid idRegistro, T registroEditado);
+
+    public bool ExcluirRegistro(Guid idRegistro);
+
+    public List<T> SelecionarRegistros();
+
+    public T SelecionarRegistroPorId(Guid idRegistro);
+}
