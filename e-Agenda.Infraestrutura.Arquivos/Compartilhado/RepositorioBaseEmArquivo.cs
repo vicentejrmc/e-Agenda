1794de7366a3b8c4affd64dffaa800002d878dce
Iagodizlla <iagoschlemper@gmail.com>
2025-06-16 15:50:28 -0300

    implementado Repositorio base

---
+++ e-Agenda.Infraestrutura.Arquivos/Compartilhado/RepositorioBaseEmArquivo.cs
@@ -0,0 +1,74 @@
+﻿using ControleDeBar.Dominio.Compartilhado;
+
+namespace ControleDeBar.Infraestrura.Arquivos.Compartilhado;
+
+public abstract class RepositorioBaseEmArquivo<T> where T : EntidadeBase<T>
+{
+    protected ContextoDados contexto;
+    protected List<T> registros = new List<T>();
+
+    protected RepositorioBaseEmArquivo(ContextoDados contexto)
+    {
+        this.contexto = contexto;
+
+        registros = ObterRegistros();
+    }
+
+    protected abstract List<T> ObterRegistros();
+
+    public void CadastrarRegistro(T novoRegistro)
+    {
+        registros.Add(novoRegistro);
+
+        contexto.Salvar();
+    }
+
+    public bool EditarRegistro(Guid idRegistro, T registroEditado)
+    {
+        foreach (T item in registros)
+        {
+            if (item.Id == idRegistro)
+            {
+                item.AtualizarRegistro(registroEditado);
+
+                contexto.Salvar();
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool ExcluirRegistro(Guid idRegistro)
+    {
+        T registroSelecionado = SelecionarRegistroPorId(idRegistro);
+
+        if (registroSelecionado != null)
+        {
+            registros.Remove(registroSelecionado);
+
+            contexto.Salvar();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public List<T> SelecionarRegistros()
+    {
+        return registros;
+    }
+
+    public T SelecionarRegistroPorId(Guid idRegistro)
+    {
+        foreach (T item in registros)
+        {
+            if (item.Id == idRegistro)
+                return item;
+        }
+
+        return null;
+    }
+}
