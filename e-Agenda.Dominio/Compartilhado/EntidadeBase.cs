6f3ae374b403ba7bc8ce14c1f184278f58e85f85
Vicente Junior <vicente_pc@outlook.com>
2025-06-17 14:34:05 -0300

    Implementado Editar Contato - Corrigo bug de Geração de ID

--- e-Agenda.Dominio/Compartilhado/EntidadeBase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ControleDeBar.Dominio.Compartilhado;
-
-public abstract class EntidadeBase<T>
-{
-    public Guid Id { get; set; }
-
-    public abstract void AtualizarRegistro(T registroEditado);
-}
