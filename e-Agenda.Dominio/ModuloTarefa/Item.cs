e274ba152ccc0194d01dccb255c264d343138e3f
Iagodizlla <iagoschlemper@gmail.com>
2025-06-17 09:13:42 -0300

    Implementado basico dos modulos de tarefa

---
+++ e-Agenda.Dominio/ModuloTarefa/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace e_Agenda.Dominio.ModuloTarefa;
+
+public class Item
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Titulo { get; set; }
+    public string StatusConclusao { get; set; }
+    public Tarefa Tarefa { get; set; }
+
+    Item(string titulo, string statusConclusao, Tarefa tarefa)
+    {
+        Titulo = titulo;
+        StatusConclusao = statusConclusao;
+        Tarefa = tarefa;
+    }
+}
