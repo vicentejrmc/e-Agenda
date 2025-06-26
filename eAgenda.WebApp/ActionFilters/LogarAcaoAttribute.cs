2acadf2f10dbb2fa904af575ccbdfff20a58f1f1
Vicente Junior <vicente_pc@outlook.com>
2025-06-26 16:19:23 -0300

    Implementado ActionFilters

---
+++ eAgenda.WebApp/ActionFilters/LogarAcaoAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace eAgenda.WebApp.ActionFilters
+{
+    public class LogarAcaoAttribute : ActionFilterAttribute
+    {
+        private readonly ILogger<LogarAcaoAttribute> logger;
+
+        public LogarAcaoAttribute(ILogger<LogarAcaoAttribute> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            var resultado = context.Result;
+
+            if(resultado is ViewResult viewResult && viewResult.Model is not null)
+            {
+                logger.LogInformation(
+                    "Açãp de endpoint execultada com sucesso! {@Modelo}", viewResult.Model);
+            }
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
---
+++ eAgenda.WebApp/ActionFilters/ValidarModeloAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace eAgenda.WebApp.ActionFilters
+{
+    public class ValidarModeloAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var modelState = context.ModelState;
+
+            if (!modelState.IsValid)
+            {
+                var controller = (Controller)context.Controller;
+
+                var viewModel = context.ActionArguments
+                    .Values
+                    .FirstOrDefault(x => x.GetType().Name.EndsWith("ViewModel"));
+
+                context.Result = controller.View(viewModel);
+            }
+        }
+    }
+}
