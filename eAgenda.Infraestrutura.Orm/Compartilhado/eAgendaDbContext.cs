6a9f5985fa473cd27ae2f00f97cef781717ae2cf
Vicente Junior <vicente_pc@outlook.com>
2025-07-16 13:34:57 -0300

    Deletado ModulosORM  a serem ajustados

--- eAgenda.Infraestrutura.Orm/Compartilhado/eAgendaDbContext.cs
+++ eAgenda.Infraestrutura.Orm/Compartilhado/eAgendaDbContext.cs
@@ -1,4 +1,5 @@
-﻿using eAgenda.Dominio.ModuloCompromisso;
+﻿using eAgenda.Dominio.ModuloCategoria;
+using eAgenda.Dominio.ModuloCompromisso;
 using eAgenda.Dominio.ModuloContato;
 using eAgenda.Dominio.ModuloTarefa;
 using eAgenda.Infraestrutura.Orm.ModuloContato;
@@ -12,7 +13,7 @@ namespace eAgenda.Infraestrutura.Orm.Compartilhado
         public DbSet<Contato> Contatos { get; set; } 
         public DbSet<Compromisso> Compromissos { get; set; } 
         public DbSet<Tarefa> Tarefas { get; set; } 
-        //public DbSet<Item> Items { get; set; } 
+        public DbSet<Categoria> Categorias { get; set; }
 
         public eAgendaDbContext(DbContextOptions options) : base(options)
         {
